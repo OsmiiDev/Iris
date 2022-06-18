cbacf3a18cb9b75e2865be94536ad1271af8c175
Osmii <76014006+OsmiiDev@users.noreply.github.com>
2022-06-17 22:25:41 -0500

    changed /shards to /src

--- iris.js
+++ iris.js
@@ -1,8 +1,9 @@
 // eslint-disable-next-line
+process.env.token = "OTU3NDA5NzMwOTU4MDg2MjU0.GwdAKi.LE64O76EYOD3A1LE4wKK9y9gvLDtJuZypJjcNY";
 
 const {ShardingManager} = require("discord.js");
 
-const manager = new ShardingManager("./shards/iris.js", {token: process.env.token});
+const manager = new ShardingManager("./src/iris.js", {token: process.env.token});
 
 manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
 
